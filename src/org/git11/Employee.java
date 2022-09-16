5f2205c19b1dd6f43f50eef7dff72e776b064d8d
githupavi <pavisara0610@gmail.com>
2022-09-16 05:07:17 -0200

    first git

---
+++ src/org/git11/Employee.java
@@ -0,0 +1,22 @@
+package org.git11;
+
+
+public class Employee {
+	private void empName() {
+
+		System.out.println("pavithra");
+		
+	}
+	private void empMobile() {
+
+		System.out.println("9786092849");
+		
+	}
+	public static void main(String[] args) {
+		Employee emp=new Employee();
+		emp.empName();
+		emp.empMobile();
+	}
+	
+
+}
